f257aeda6fed90f1a634507b79b09192ebba903b
Saif Shahriar <sh.saif@proton.me>
2025-02-03 00:34:58 +0600

    Add dummy Profile route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Create } from "./components/Create";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./components/Home";
 import { Dashboard } from "./components/Dashboard";
+import { Profile } from "./components/Profile";
 import { Toaster } from "react-hot-toast";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
 					<Route path="/login" element={<Login />} />
 					<Route path="/signup" element={<Create />} />
 					<Route path="/dashboard" element={<Dashboard />} />
+					<Route path="/profile" element={<Profile />} />
 				</Routes>
 			</Router>
 			<Toaster />
